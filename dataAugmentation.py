0fd496cccd506323122cdc17a1b1430669103d5b
Karen Stephany <karenquito2004@gmail.com>
2025-07-11 14:18:54 -0500

    Mejora con modelo multietiqueta

--- dataAugmentation.py
+++ dataAugmentation.py
@@ -17,7 +17,13 @@ output_dir = 'Dataset-SupermarketImages/Productos/Train_Aumentado'
 
 os.makedirs(output_dir, exist_ok=True)
 
+# Solo las clases deseadas
+clases_a_duplicar = ['doritos', 'cocaCola']
+
 for clase in tqdm(os.listdir(input_dir)):
+    if clase not in clases_a_duplicar:
+        continue  # saltar otras clases
+
     clase_dir = os.path.join(input_dir, clase)
     output_clase_dir = os.path.join(output_dir, clase)
     os.makedirs(output_clase_dir, exist_ok=True)
@@ -28,11 +34,11 @@ for clase in tqdm(os.listdir(input_dir)):
         x = img_to_array(img)
         x = x.reshape((1,) + x.shape)
 
-        # Guardar 3 versiones aumentadas por imagen (puedes cambiar ese número)
+        # Guardar 3 versiones aumentadas por imagen
         i = 0
         for batch in augmentador.flow(x, batch_size=1):
             save_path = os.path.join(output_clase_dir, f"aug_{i}_{img_file}")
             save_img(save_path, batch[0])
             i += 1
-            if i >= 3:
+            if i >= 6:
                 break
